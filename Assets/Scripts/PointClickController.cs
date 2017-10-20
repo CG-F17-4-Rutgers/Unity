43c2220bdb794d7110c7d67c3337d69f65c4b1fa
Taichi Aritomo <taichiaritomo@gmail.com>
2017-10-20 01:30:36 -0400

    B2-2 and B2-3 Environment, Controls, and Agents

--- Assets/Scripts/PointClickController.cs
+++ Assets/Scripts/PointClickController.cs
@@ -8,7 +8,7 @@ public class PointClickController : MonoBehaviour {
 
 	private Ray shootRay;        // Ray cast from mouse position (at camera)
 	private RaycastHit shootHit; // GameObject hit by shootRay
-	private GameObject selectedGameObject;
+	private GameObject activeGameObject;
 
 
 	// Use this for initialization
@@ -18,8 +18,39 @@ public class PointClickController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// ignore clicks on Event System
+		if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject ())
+			return;
+
 		// Handle I/O Pointer Events
-		Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//		Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
 		RaycastHit hit;
+
+		// Select
+		if (Input.GetButtonDown ("Fire2"))
+		{
+			if (Physics.Raycast (ray, out hit, raycastDistance))
+			{
+				if (hit.collider.CompareTag ("Character"))
+				{
+					GameObject selectedGameObject = hit.transform.gameObject;
+					selectedGameObject.SendMessage ("toggleActive");
+					activeGameObject = (activeGameObject == null ? selectedGameObject : null);
+				}
+			}
+		}
+
+		// Act
+		if (Input.GetButtonDown ("Fire1"))
+		{
+			if (Physics.Raycast (ray, out hit, raycastDistance))
+			{
+				if (activeGameObject != null)
+				{
+					activeGameObject.SendMessage ("moveTo", hit.point);
+				}
+			}
+		}
 	}
 }
