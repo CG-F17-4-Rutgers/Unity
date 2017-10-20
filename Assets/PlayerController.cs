a7e3974848f2c4b461c3eaaeb9c631b1427dfc09
Justin Wu <JustinWu626@gmail.com>
2017-10-20 15:27:43 -0400

    Working Animator Added

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Animator anim;
+	private readonly int movingHash = Animator.StringToHash("Moving");
+	private readonly int jumpHash = Animator.StringToHash("Jump");
+	private readonly int turningHash = Animator.StringToHash("Turning");
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		if (Mathf.Abs(vertical) > 0.1f) {
+			anim.SetBool (movingHash, true);
+		}
+
+		if (horizontal != 0.0f && Mathf.Abs(vertical) < 0.1f) {
+			anim.SetBool (turningHash, true);
+		} else {
+			anim.SetBool (turningHash, false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (!Input.GetKey (KeyCode.S)) {
+				anim.SetTrigger (jumpHash);
+			}
+		}
+	}
+}
---
+++ Assets/WalkingSMB.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class WalkingSMB : StateMachineBehaviour
+{
+	public float m_Damping = 0.15f;
+
+
+	private readonly int m_HashHorizontalPara = Animator.StringToHash ("Horizontal");
+	private readonly int m_HashVerticalPara = Animator.StringToHash ("Vertical");
+	private readonly int m_HashStrafePara = Animator.StringToHash ("Strafe");
+	private readonly int m_HashMovingPara = Animator.StringToHash ("Moving");
+	private readonly int m_HashRunningPara = Animator.StringToHash ("Running");
+	private readonly int m_HashStrafingPara = Animator.StringToHash ("Strafing");
+
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		float strafe = Input.GetAxis ("Strafe");
+
+		Vector2 input = new Vector2(horizontal, vertical).normalized;
+
+		animator.SetFloat (m_HashHorizontalPara, input.x, m_Damping, Time.deltaTime);
+		animator.SetFloat (m_HashVerticalPara, input.y, m_Damping, Time.deltaTime);
+		animator.SetFloat (m_HashStrafePara, strafe, m_Damping, Time.deltaTime);
+
+		if (Mathf.Abs (vertical) > 0.1f) {
+			animator.SetBool (m_HashMovingPara, true);
+		} else {
+			animator.SetBool (m_HashMovingPara, false);
+		}
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			animator.SetBool (m_HashRunningPara, true);
+		} else {
+			animator.SetBool (m_HashRunningPara, false);
+		}
+		if (strafe != 0.0) {
+			animator.SetBool (m_HashStrafingPara, true);
+		} else {
+			animator.SetBool (m_HashStrafingPara, false);
+		}
+	}
+}
