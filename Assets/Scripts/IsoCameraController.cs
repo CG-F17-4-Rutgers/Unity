abcbe1c5b45d23189e53a380ad030bab19dbadb3
Taichi Aritomo <taichiaritomo@gmail.com>
2017-10-19 21:07:45 -0400

    Add isocamera script

---
+++ Assets/Scripts/IsoCameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoCameraController : MonoBehaviour {
+
+	public Camera cam;
+	private Transform g_cam;
+
+	private Vector3 isoForward = new Vector3(1.0f, 0.0f, 1.0f);
+	private float ANGLE_PI_DIV_4 = 45 * Mathf.Deg2Rad;
+
+	public int panSpeed;
+	public int scrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+        panSpeed = 6;
+        scrollSpeed = 100;
+
+		g_cam = cam.transform;
+		g_cam.LookAt(Vector3.zero); // Default look at origin
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		// Panning
+		float inputX = Input.GetAxis ("Horizontal") * panSpeed;
+		float inputZ = Input.GetAxis ("Vertical") * panSpeed;
+		float moveX = Mathf.Cos (-ANGLE_PI_DIV_4) * inputX - Mathf.Sin (-ANGLE_PI_DIV_4) * inputZ;
+		float moveZ = Mathf.Sin (-ANGLE_PI_DIV_4) * inputX + Mathf.Cos (-ANGLE_PI_DIV_4) * inputZ;
+		g_cam.Translate (new Vector3 (moveX, 0, moveZ) * Time.deltaTime, Space.World);
+
+		// Zooming
+		float inputScroll = Input.GetAxis ("Mouse ScrollWheel") * scrollSpeed;
+		g_cam.Translate (new Vector3 (0, 0, inputScroll) * Time.deltaTime, Space.Self);
+
+		// WARNING: Floating point errors may result in accumulating errors of camera angle.
+		// TODO: Regularly adjust camera transform LookAt to maintain isometric angle.
+	}
+}
---
+++ Assets/Scripts/PointClickController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointClickController : MonoBehaviour {
+
+	public float raycastDistance;
+
+	private Ray shootRay;        // Ray cast from mouse position (at camera)
+	private RaycastHit shootHit; // GameObject hit by shootRay
+	private GameObject selectedGameObject;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Handle I/O Pointer Events
+		Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
+		RaycastHit hit;
+	}
+}
